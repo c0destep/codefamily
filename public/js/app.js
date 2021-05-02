826ecb9e391cdc59ca87982c39999684084cc34a
phytoline <phytolinedev@gmail.com>
2021-05-02 14:31:21 -0300

    ajustes

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,15 @@
-window.addEventListener("load", () => {
-    let icon = document.querySelectorAll(".icon-box");
+// window.addEventListener("scroll", () => {
+//     let icon = document.querySelectorAll(".icon-box");
 
-    for (let i = 0; i < icon.length; i++) {
-        let top = Math.floor(Math.random() * 100);
-        let left = Math.floor(Math.random() * 100);
-        let right = Math.floor(Math.random() * 100);
-        let index = Math.floor(Math.random() * 10);
+//     for (let i = 0; i < icon.length; i++) {
+//         let top = Math.floor(Math.random() * 100);
+//         let left = Math.floor(Math.random() * 100);
+//         let right = Math.floor(Math.random() * 100);
+//         let index = Math.floor(Math.random() * 10);
 
-        icon[i].setAttribute('style', 'top:' + top + '%;left:' + left + '%;right:' + right + '%;z-index:' + index + ';')
-    }
-});
+//         icon[i].setAttribute('style', 'top:' + top + '%;left:' + left + '%;right:' + right + '%;z-index:' + index + ';')
+//     }
+// });
 
 window.addEventListener("scroll", () => {
     const header = document.querySelector("header");
